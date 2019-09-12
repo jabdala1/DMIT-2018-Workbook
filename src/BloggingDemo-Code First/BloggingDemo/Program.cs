b03531ea200f90f5d364998c7e6555a8996b64c2
Janne Abdala <jabdala1@nait.ca>
2019-09-12 10:29:10 -0600

    Added Context Class

--- src/BloggingDemo-Code First/BloggingDemo/Program.cs
+++ src/BloggingDemo-Code First/BloggingDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BloggingDemo.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,7 @@ namespace BloggingDemo
         }
 
     }
+
     namespace Entities
     {
         public class Blog
@@ -24,7 +27,7 @@ namespace BloggingDemo
 
             // Navigation Property
             public virtual ICollection<Post> Posts { get; set; }
-        }
+        } // End of Blog class
 
         public class Post
         {
@@ -35,6 +38,19 @@ namespace BloggingDemo
 
             // Navigation Property
             public virtual Blog Blog { get; set; }
-        }
+        } // End of Post class
+    } // End of Entities namespace
+
+    namespace DAL
+    {
+        public class BloggingContext : DbContext
+        {
+            public BloggingContext() : base("name=BlogDb")
+            {
+            }
+
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
     }
 }
+
