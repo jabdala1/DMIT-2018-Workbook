c2aa71df0709eda5207cedee579a2dd5a35af5d3
Janne Abdala <jabdala1@nait.ca>
2019-09-12 10:18:28 -0600

    Added Blog and Post entity

---
+++ src/BloggingDemo-Code First/BloggingDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    namespace Entities
+    {
+        public class Blog
+        {
+            // "Primary Key", by convention
+            public int BlogID { get; set; }
+            public string Name { get; set; }
+
+            // Navigation Property
+            public virtual ICollection<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            // "Primary Key", by convention
+            public int PostID { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            // Navigation Property
+            public virtual Blog Blog { get; set; }
+        }
+    }
+}
