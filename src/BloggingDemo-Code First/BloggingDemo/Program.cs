ada6b3f063404fe7e27db999fecdfc9399a23e77
Janne Abdala <jabdala1@nait.ca>
2019-09-12 10:59:14 -0600

    Finished Week #2 Day 2

--- src/BloggingDemo-Code First/BloggingDemo/Program.cs
+++ src/BloggingDemo-Code First/BloggingDemo/Program.cs
@@ -1,10 +1,12 @@
-﻿using BloggingDemo.Entities;
+﻿using BloggingDemo.DAL;
+using BloggingDemo.Entities;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace BloggingDemo
 {
@@ -12,9 +14,48 @@ namespace BloggingDemo
     {
         static void Main(string[] args)
         {
-
+            var app = new Program();
+            // . is Member Acess Operator
+            app.Run();
+            Write("Press any key to continue...");
+            ReadKey();
         }
 
+        private void Run()
+        {
+            Welcome();
+            // Display the blogs in the database
+            using (var context = new BloggingContext())
+            {
+                // Create and save a new blog
+                Write("Enter a name for a new blog: ");
+                var name = ReadLine();
+
+                var blog = new Blog
+                {
+                    Name = name
+                };
+
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+
+                // Display all blogs from the database
+                List<Blog> blogs = context.Blogs.ToList();
+                DisplayBlogNames(blogs);
+
+            }
+        }
+
+        private void DisplayBlogNames(List<Blog> blogs)
+        {
+            foreach (var item in blogs)
+                WriteLine(item.Name);
+        }
+
+        private void Welcome()
+        {
+            WriteLine("Blogging Demo Program");
+        }
     }
 
     namespace Entities
@@ -51,6 +92,7 @@ namespace BloggingDemo
 
             public DbSet<Blog> Blogs { get; set; }
             public DbSet<Post> Posts { get; set; }
+        }
     }
 }
 
