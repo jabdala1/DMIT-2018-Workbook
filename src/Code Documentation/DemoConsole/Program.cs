8d900b2f5b99fcba25853705c6664034d134caa9
jabdala1 <46543609+jabdala1@users.noreply.github.com>
2019-10-29 16:30:12 -0600

    Demo of XML comments in code

---
+++ src/Code Documentation/DemoConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shipInfo = new ShippingDirections();
+            ShipOrder(5, shipInfo, null);
+        }
+
+        /// <summary>Process the order shipment</summary>
+        /// <param name="orderid">The ID of the order beign shipped</param>
+        /// <param name="shipping">The <see cref="ShippingDirections"/> of the items being shipped</param>
+        /// <param name="items"></param>
+        static void ShipOrder(int orderid, ShippingDirections shipping, List<ShippedItem> items)
+        {
+
+        }
+    }
+
+    /// <summary>Represents information about the shipper and tracking /freight details for shipping an order.</summary>
+    public class ShippingDirections
+    {
+        /// <summary>Primary Key of the Shipper</summary>
+        public int ShipperId { get; set; }
+        /// <summary>Tracking Code for checking shipment progress online</summary>
+        public string TrackingCode { get; set; }
+        /// <summary>Freight charges that the shipper makes to the supplier</summary>
+        public decimal? FreightCharge { get; set; }
+    }
+    public class ShippedItem
+    {
+        public int ProductID { get; set; }
+        public int ShipQuantity { get; set; }
+    }
+}
